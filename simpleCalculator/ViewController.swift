f7f02210900b5a0aec614b4b6ea81274030782c3
aybatu <aybatu.kerkukluoglu@gmail.com>
2023-09-09 09:39:22 +0100

    Initial Commit

---
+++ simpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleCalculator
+//
+//  Created by Aybatu KERKUKLUOGLU on 09/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
